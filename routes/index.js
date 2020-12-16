331fbab2ee496bbc0878d40629eee5584c4dd0aa
markuslat <62180247+markuslat@users.noreply.github.com>
2020-12-16 17:14:03 +0200

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var router = express.Router();
+
+/*GET home page. */
+router.get("/", function (req, res, next) {
+  // Retreiving the posts from the global var
+  var post_list = req.app.get("poststore");
+
+  res.render("index", {
+    title: "MICROJUNK",
+    post_list: post_list
+  });
+});
+
+router.post("/getin", function (req, res, next) {
+  res.redirect("/signup");
+});
+
+module.exports = router;
